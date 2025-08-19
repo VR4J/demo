8a0516a0c551dad869e169fdf57c7d9b513d6167
Joery Vreijsen <joery_vreijsen@hotmail.com>
2025-08-19 09:25:04 +0200

    Update PersonRepository.java

--- src/main/java/com/example/demo/PersonRepository.java
+++ src/main/java/com/example/demo/PersonRepository.java
@@ -1,8 +1,9 @@
 package com.example.demo;
 
-import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
+import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface PersonRepository extends CrudRepository<Person, String> {
@@ -10,5 +11,5 @@ public interface PersonRepository extends CrudRepository<Person, String> {
     @Query(value = "{ 'forename': ?0 }", sort = "{ 'forename': 1 }")
     Optional<Person> getFirstPersonByForename(String forename);
 
-    List<Person> finByForename(String forename)
+    List<Person> finByForename(String forename);
 }
